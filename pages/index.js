83723d31e6496cc0bbde4b223a0dcb427a106720
Akshay <akshayyadav04031998@gmail.com>
2021-06-28 22:44:22 +0530

    Outputting a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/04/Bara-Imambara.jpg",
+    address: "Bada Imaambada,Lucknow",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/04/Bara-Imambara.jpg",
+    address: "Bada Imaambada,Lucknow",
+    description: "This is a second meetup",
+  },
+];
 const index = () => {
-  return <p>Index Page</p>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 export default index;
