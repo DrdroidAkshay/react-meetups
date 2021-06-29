02c34d9b0e1c08c3063bfa9534e1d4a884dac3d7
Akshay <akshayyadav04031998@gmail.com>
2021-06-29 13:49:45 +0530

    Working with mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,21 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+async const handler = (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
     const { title, image, address, description } = data;
+
+    const client= await MongoClient.connect(
+      "mongodb+srv://Akshay:Akshay123@cluster0.sew4n.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db=client.db();
+    const meetupsCollection=db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message:"Meetup Inserted"});
   }
 };
 export default handler;
