1dd0905d192c435cb021ccc75e51948a2efa7f86
Akshay <akshayyadav04031998@gmail.com>
2021-06-29 14:59:38 +0530

    Getting data from database

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -22,18 +23,33 @@ const index = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
-  return {
-    props: { meetups: DUMMY_MEETUPS },
-  };
-}
-
-// export async function getStaticProps() {
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
 //   return {
 //     props: { meetups: DUMMY_MEETUPS },
-//     revalidate: 10,
 //   };
 // }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Akshay:Akshay123@cluster0.sew4n.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        description: meetup.description,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 10,
+  };
+}
 export default index;
